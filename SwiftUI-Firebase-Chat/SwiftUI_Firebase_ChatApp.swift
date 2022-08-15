5382e565341a80d55903b756398a91052641619c
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-08-15 21:11:29 +0530

    Initial Commit

---
+++ SwiftUI-Firebase-Chat/SwiftUI_Firebase_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Firebase_ChatApp.swift
+//  SwiftUI-Firebase-Chat
+//
+//  Created by Apekshik Panigrahi on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Firebase_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
